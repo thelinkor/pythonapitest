952455903924cbba6d1bef57ed1b795030e5a65b
Ove Ahlman <ove.ahlman@ladyluckgames.io>
2022-08-06 20:34:15 +0200

    Removed redundant files which previously slipped into the commits.

--- apisetup.py
+++ apisetup.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, json
 
+def page_not_found(e):
+    return "The request was sent to a page which does not exist", 404
+
 def setup():
     app = Flask(__name__)
+    app.register_error_handler(404, page_not_found)
     return app
     
--- try2.py
+++ try2.py
@@ -4,9 +4,11 @@ import signal
 import sys
 import subprocess
 import time
+from flask import Flask, request, json
 
 
-app  = apisetup.setup()
+
+app = apisetup.setup()
 
 @app.route('/')
 def hello():
@@ -14,19 +16,19 @@ def hello():
 
 @app.route('/post_json', methods=['POST'])
 def process_json():
-    c = request.headers.get('Contet-Type')
     data = request.get_json()
-    #data = json.loads(request.data)
-    v =subprocess.check_output([data['command'], '-l'])
+    v = subprocess.check_output(getCommandArrayFromRequest(data))
     time.sleep(5)
     print(v)
     return v
-    content_type = request.headers.get('Content-Type')
-    if (content_type == 'application/json'):
-        json = request.get_json()
-        return json
-    else:
-        return 'Content-Type not supported!'
+
+def getCommandArrayFromRequest(data):
+    out = ['ls']
+    if 'path' in data.keys():
+        out = out + [data['path']]
+    if 'arguments' in data.keys():
+        out = out + data['arguments']
+    return out
 
 def signal_handler(sig, frame):
     print('You pressed Ctrl+C!')
